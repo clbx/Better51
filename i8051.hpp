52e7352f4d14de97881ace6043fcc128cd3ba788
Clay Buxton <clay@clbx.io>
2019-07-23 18:27:25 -0400

    skeleton

--- i8051.hpp
+++ i8051.hpp
@@ -5,14 +5,19 @@ class i8051{
     private:
 
         uint16_t pc; //Program Counter
+        uint8_t sp; //Stack Pointer
+        uint16_t dp; //Data Pointer (probably never used in this scenario)
 
         uint8_t memory[128]; //128 byte RAM
-        uint8_t rom[64000]; //64k byte ROM
+        uint8_t rom[4096]; //4k byte ROM
 
         uint8_t a; //Accumulator
         uint8_t b; //B register
 
         uint8_t psw; //Program status word
+
+        uint16_t timer0; //First timer
+        uint16_t timer1; //Second timer
 
         void execute(uint8_t);
 
